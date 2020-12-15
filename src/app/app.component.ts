3cea7ea24f29507176ef7825b784c85aa3fb5b06
Azahar Hossain <azahar.hossain@ampada.local>
2020-12-15 08:50:04 +0100

    254-255 : Post and Get requests

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
+    this.http
+      .post(
+        "https://ng-basic-angular-http-default-rtdb.firebaseio.com/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+  private fetchPosts(): void {
+    this.http
+      .get(
+        "https://ng-basic-angular-http-default-rtdb.firebaseio.com/posts.json"
+      )
+      .subscribe((_posts) => {
+        console.log(_posts);
+      });
+  }
+}
